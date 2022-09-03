9d9cc26752cc8a3ed5114537a0bbd0738b89fcd9
HiroMinase <lostsymbol612@gmail.com>
2022-09-04 01:29:17 +0900

    ウィジェット群をそのまま写真にする方式を試した

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
+import 'dart:core';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart' as picker;
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+// カメラ経由かギャラリー経由かを示すフラグ
+enum FileMode{
+  takePicture,
+  importFromGallery,
+}
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +21,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +36,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +43,186 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  File? _pickImage;
+  final _picker = picker.ImagePicker();
+  List<Widget> stampWidgets = [];
+  final _globalKey = GlobalKey();
 
-  void _incrementCounter() {
+  //  画像取得処理
+  Future getImage(FileMode fileMode) async {
+    late final dynamic pickedFile;
+
+    // カメラからとギャラリーからの2通りの画像取得（パスの取得）を設定
+    if (fileMode == FileMode.takePicture) {
+      pickedFile = await _picker.pickImage(source: picker.ImageSource.camera, imageQuality: 100);
+    } else {
+      pickedFile = await _picker.pickImage(source: picker.ImageSource.gallery, imageQuality: 100);
+    }
+
+    if (pickedFile != null) {
+      setState(() {
+        _pickImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  addStampWidget(String fileName) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      stampWidgets.add(
+        StatefulDragArea(
+          fileName: fileName,
+          child: Image.asset(
+            "assets/stickers/$fileName",
+            width: 60,
+            height: 60,
+          )
+        )
+      );
+    });
+  }
+
+  // RepaintBoundary の key を渡す
+  void convertWidgetToImage(GlobalKey widgetGlobalKey) async {
+    // RenderObjectを取得
+    final RenderRepaintBoundary boundary = widgetGlobalKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
+    // RenderObject を dart:ui の Image に変換する
+    final ui.Image image = await boundary.toImage();
+    final ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List buffer = byteData!.buffer.asUint8List();
+    ImageGallerySaver.saveImage(buffer, quality: 100);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: RepaintBoundary(
+                key: _globalKey,
+                child: Stack(
+                  children: [
+                    if (_pickImage != null) Image.file(_pickImage!, fit: BoxFit.cover, width: double.infinity),
+                    ...stampWidgets,
+                  ],
+                ),
+              ),
+            ),
+            if (_pickImage == null) GestureDetector(
+              onTap: () {
+                getImage(FileMode.importFromGallery);
+              },
+              child: const Text("読み込み"),
+            ),
+            if (_pickImage != null) Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    addStampWidget("guratan_camera.png");
+                  },
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(width: 1.0, color: Colors.black),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Text("スタンプを追加")
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    convertWidgetToImage(_globalKey);
+                  },
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(width: 1.0, color: Colors.black),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Text("画像を保存")
+                    ),
+                  ),
+                ),
+              ]
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class StatefulDragArea extends StatefulWidget {
+  const StatefulDragArea({Key? key, required this.child, required this.fileName}) : super(key: key);
+
+  final Widget child;
+  final String fileName;
+
+  @override
+  State<StatefulDragArea> createState() => _DragAreaStateState();
+}
+
+class _DragAreaStateState extends State<StatefulDragArea> {
+  final Size windowSize = MediaQueryData.fromWindow(ui.window).size;
+  late Offset position = Offset((windowSize.width / 2 - 30), (windowSize.height / 2 - 30));
+  double prevScale = 1;
+  double scale = 1;
+
+  updateScale(double zoom) {
+    setState(() {
+      scale = prevScale * zoom;
+    });
+  }
+
+  commitScale() {
+    setState(() {
+      prevScale = scale;
+    });
+  }
+
+  updatePosition(Offset newPosition) {
+    setState(() {
+      position = Offset(newPosition.dx, newPosition.dy - 103);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return GestureDetector(
+      onScaleUpdate: (details) => updateScale(details.scale),
+      onScaleEnd: (_) => commitScale(),
+      child: Stack(
+        children: [
+          Positioned(
+            left: position.dx,
+            top: position.dy,
+            child: Draggable(
+              maxSimultaneousDrags: 1,
+              feedback: widget.child,
+              childWhenDragging: Container(),
+              onDragEnd: (details) {
+                updatePosition(details.offset);
+              },
+              child: Transform.scale(
+                scale: scale,
+                child: widget.child,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

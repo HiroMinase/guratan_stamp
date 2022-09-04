18cfe1d3daf515518f367434fc1093294dcfc07e
HiroMinase <lostsymbol612@gmail.com>
2022-09-04 23:52:36 +0900

    保存が完了した時に toast を表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:extended_image/extended_image.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
@@ -45,11 +47,13 @@ class _AdvancedExampleState extends State<AdvancedExample> {
   final _picker = ImagePicker();
   late StickIt _stickIt;
   File? _image;
+  bool _isSaving = false;
 
   @override
   Widget build(BuildContext context) {
     double bottomPadding = MediaQuery.of(context).size.height / 4;
-    double leftPadding = MediaQuery.of(context).size.width / 12;
+    double leftPadding = MediaQuery.of(context).size.width / 10;
+    double boxSize = 56.0;
     _stickIt = StickIt(
       stickerList: [
         Image.asset(
@@ -68,10 +72,10 @@ class _AdvancedExampleState extends State<AdvancedExample> {
         Image.asset('assets/stickers/guratan_ehime_01.png'),
       ],
       key: UniqueKey(),
-      panelHeight: 160,
-      panelBackgroundColor: Colors.white,
-      panelStickerBackgroundColor: Theme.of(context).primaryColorLight,
-      stickerSize: 50,
+      panelHeight: 170,
+      panelBackgroundColor: ColorTable.primaryWhiteColor,
+      panelStickerBackgroundColor: ColorTable.primaryBlueColor,
+      stickerSize: 100,
       child: _image == null ? Image.network(_background) : Image.file(_image!, fit: BoxFit.cover),
     );
 
@@ -99,16 +103,28 @@ class _AdvancedExampleState extends State<AdvancedExample> {
                     final uniqueIdentifier = const Uuid().v1();
                     final file = await File('$path/$uniqueIdentifier.png').create();
                     file.writeAsBytesSync(image);
-                    GallerySaver.saveImage(file.path, albumName: 'guratan').then((value) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text("画像を保存しました！"),
-                      ));
-                    });
+                    GallerySaver.saveImage(file.path, albumName: 'guratan');
+
+                    Fluttertoast.showToast(
+                      msg: "保存が完了しました！",
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 2,
+                      backgroundColor: ColorTable.primaryWhiteColor,
+                      textColor: ColorTable.primaryBlackColor,
+                      fontSize: 16.0,
+                    );
                   },
-                  child: const Icon(
-                    Icons.save,
-                    color: Colors.white,
-                    size: 36,
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: ColorTable.primaryBlackColor.withOpacity(0.4),
+                    ),
+                    child: const Icon(
+                      Icons.save,
+                      color: Colors.white,
+                      size: 36,
+                    ),
                   ),
                 ),
                 const SizedBox(
@@ -119,13 +135,37 @@ class _AdvancedExampleState extends State<AdvancedExample> {
                   onTap: () {
                     generateModal(context);
                   },
-                  child: const Icon(
-                    Icons.image,
-                    color: Colors.white,
-                    size: 36,
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: ColorTable.primaryBlackColor.withOpacity(0.4),
+                    ),
+                    child: const Icon(
+                      Icons.image,
+                      color: Colors.white,
+                      size: 36,
+                    ),
                   ),
                 ),
               ],
+            ),
+          ),
+          if (_isSaving == true) Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: ColorTable.primaryBlackColor.withOpacity(0.5)
+              ),
+              child: const SizedBox(
+                width: 100,
+                height: 100,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(ColorTable.primaryWhiteColor),
+                  ),
+                ),
+              ),
             ),
           ),
         ],
@@ -133,22 +173,23 @@ class _AdvancedExampleState extends State<AdvancedExample> {
     );
   }
 
-  // image_picker で取得した画像を _image に保存
-  Future getImage(ImageSource source) async {
-    final pickedFile = await _picker.pickImage(source: source);
-    if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
-    }
-  }
-
+  // 画像を散り込む方法を選択するモーダルを表示
   void generateModal(BuildContext context) {
+    // image_picker で取得した画像を _image に保存
+    Future getImage(ImageSource source) async {
+      final pickedFile = await _picker.pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          _image = File(pickedFile.path);
+        });
+      }
+    }
+
     showModalBottomSheet<void>(
       context: context,
       builder: (BuildContext context) {
         return Container(
-          height: 200,
+          height: 180,
           color: Colors.white,
           child: Center(
             child: Column(
